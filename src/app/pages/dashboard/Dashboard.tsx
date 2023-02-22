cf21f9b7f277e9b3bca6a7b601a99d05be7bd03d
Luana Portella Rodriguez <luana.portella@penseavanti.com.br>
2023-02-22 14:54:32 -0300

    create context

--- src/app/pages/dashboard/Dashboard.tsx
+++ src/app/pages/dashboard/Dashboard.tsx
@@ -2,9 +2,9 @@ import { Link } from "react-router-dom";
 
 export const Dashboard = () => {
     return (
-        <div>
+        <div className="divDashboard">
         <p>Dashboard</p>
-        <Link to="/entrar">Login</Link>
+        <Link to="/entrar">Fazer login</Link>
         </div>
     )
 }
